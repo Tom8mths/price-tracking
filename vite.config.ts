431dca689bab6c243723de0f65ec8c0e1eeea541
Matheus Satelos <matheus.satelos@gmail.com>
2025-04-03 17:21:12 -0300

    feat: configure postcss processors in the project

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import * as path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
@@ -16,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
