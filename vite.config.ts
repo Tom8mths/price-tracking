9b0c94f32260939052a72384d3ddd6bacc7e4dd4
Matheus Satelos <matheus.satelos@gmail.com>
2025-04-06 19:40:32 -0300

    fix: fix type error preventing build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    port: Number(process.env.PORT) || 3000,
     proxy: {
       '/finance-api': {
         target: 'https://api.hgbrasil.com',
